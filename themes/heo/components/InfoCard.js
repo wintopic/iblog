bb64f9cd40328b70a42fedfcc6f46238e03cfbd7
wintopic <97203022+wintopic@users.noreply.github.com>
2025-03-23 19:44:34 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -1,4 +1,3 @@
-import { ArrowRightCircle } from '@/components/HeroIcons'
 import LazyImage from '@/components/LazyImage'
 import { siteConfig } from '@/lib/config'
 import Link from 'next/link'
@@ -7,6 +6,34 @@ import CONFIG from '../config'
 import Announcement from './Announcement'
 import Card from './Card'
 
+/**
+ * 社交信息卡
+ * @param {*} props
+ * @returns
+ */
+export function InfoCard(props) {
+  const { siteInfo, notice } = props
+  const router = useRouter()
+  // 在文章详情页特殊处理
+  const isSlugPage = router.pathname.indexOf('/[prefix]') === 0
+  const url1 = siteConfig('HEO_INFO_CARD_URL1', null, CONFIG)
+  const url2 = siteConfig('HEO_INFO_CARD_URL2', null, CONFIG)
+
+  return (
+    <Card className='wow fadeInUp bg-[#4f65f0] dark:bg-yellow-600 text-white flex flex-col w-72 overflow-hidden relative'>
+      {/* 信息卡牌第一行 */}
+      <div className='flex justify-between'>
+        {/* 头像 */}
+        <div
+          className={`${isSlugPage ? 'absolute right-0 -mt-8 -mr-6 hover:opacity-0 hover:scale-150 blur' : 'cursor-pointer'} justify-center items-center flex dark:text-gray-100 transform transition-all duration-200`}>
+          <LazyImage
+            src={siteInfo?.icon}
+            className='rounded-full'
+            width={isSlugPage ? 100 : 28}
+            alt={siteConfig('AUTHOR')}
+          />
+        </div>
+      </div>
 
       {/* 公告栏 */}
       <Announcement post={notice} style={{ color: 'white !important' }} />
@@ -17,14 +44,14 @@ import Card from './Card'
           {url1 && (
             <div className='w-10 text-center bg-indigo-400 p-2 rounded-full transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
               <Link href={url1}>
-                <i className={icon1} />
+                <span>Link 1</span>
               </Link>
             </div>
           )}
           {url2 && (
             <div className='bg-indigo-400 p-2 rounded-full w-10 items-center flex justify-center transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
               <Link href={url2}>
-                <i className={icon2} />
+                <span>Link 2</span>
               </Link>
             </div>
           )}
@@ -51,7 +78,6 @@ function MoreButton() {
   return (
     <Link href={url3}>
       <div className='group bg-indigo-400 dark:bg-yellow-500 hover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white flex items-center transition-colors duration-200 py-2 px-3 rounded-full space-x-1'>
-        <ArrowRightCircle className='group-hover:stroke-black dark:group-hover:stroke-white w-6 h-6 transition-all duration-100' />
         <div className='font-bold'>{text3}</div>
       </div>
     </Link>
